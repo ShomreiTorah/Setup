b2b31fc6efefb3fe49a9f771a7680f10d1e984fe
SLaks <Schabse@Gmail.com>
2012-06-26 12:55:20 -0400

    Read XML comments

---
+++ Configurator/HelpBox.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Configurator {
+	/// <summary>
+	/// Interaction logic for HelpBox.xaml
+	/// </summary>
+	partial class HelpBox : UserControl {
+		public HelpBox() {
+			InitializeComponent();
+		}
+
+
+
+		public string MemberName {
+			get { return (string)GetValue(MemberNameProperty); }
+			set { SetValue(MemberNameProperty, value); }
+		}
+
+		public static readonly DependencyProperty MemberNameProperty =
+			DependencyProperty.Register("MemberName", typeof(string), typeof(HelpBox), new PropertyMetadata(MemberNameChanged));
+
+		static readonly XDocument docComments = XDocument.Load(Path.ChangeExtension(typeof(HelpBox).Assembly.Location, ".xml"));
+
+		static void MemberNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			var box = (HelpBox)d;
+			var memberName = e.NewValue as string;
+			if (string.IsNullOrEmpty(memberName))
+				box.text.Text = null;
+			else {
+				if (!memberName.StartsWith("Configurator."))
+					memberName = "Configurator." + memberName;
+				box.text.Text = docComments.Root.Element("Members")
+												.Elements("Member")
+												.First(m => m.Attribute("name").Value.EndsWith(":" + memberName))
+												.Element("summary")
+												.Value;
+			}
+		}
+	}
+}
--- Configurator/MainWindow.xaml.cs
+++ Configurator/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace Configurator {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window {
+	partial class MainWindow : Window {
 		public MainWindow() {
 			InitializeComponent();
 		}
