24e0d89d9ebdeafeb4b18f7c8f3964c9f96770b9
Schabse Laks <Schabse@Gmail.com>
2013-02-10 22:19:05 -0500

    Fix install script

--- Install.cmd
+++ Install.cmd
@@ -1,31 +1,36 @@
-﻿@ECHO OFF
-IF NOT "%~n0"=="Setup" (
-	ECHO ShomreiTorah/Setup must be cloned to a directory named "Setup".
+@ECHO OFF
+SETLOCAL
+where /q git
+IF %ERRORLEVEL% == 1 (
+	ECHO This setup script requires that git be in the system path.
 	GOTO :eof 
 )
-set MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+SET MSBUILD=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 IF NOT EXIST "%MSBuild%" (
 	ECHO The Shomrei Torah Suite requires .Net 4.0.
 	GOTO :eof 
 )
-pushd %~dp0
+PUSHD %~dp0\..
 SET REMOTE_BASE=https://github.com/ShomreiTorah
 
 ECHO Cloning projects
-git clone -q %REMOTE_BASE%/Libraries	Libraries
-git clone -q %REMOTE_BASE%/Utilities	Utilities
-git clone -q %REMOTE_BASE%/Billing		Applications/Billing
-git clone -q %REMOTE_BASE%/Schedulizer	Applications/Schedulizer
-git clone -q %REMOTE_BASE%/Rafflizer	Applications/Rafflizer
-git clone -q %REMOTE_BASE%/Journal		Applications/Journal
+git clone 2>nul %REMOTE_BASE%/Libraries	Libraries
+git clone 2>nul %REMOTE_BASE%/Utilities	Utilities
+git clone 2>nul %REMOTE_BASE%/Billing		Applications/Billing
+git clone 2>nul %REMOTE_BASE%/Schedulizer	Applications/Schedulizer
+git clone 2>nul %REMOTE_BASE%/Rafflizer	Applications/Rafflizer
+git clone 2>nul %REMOTE_BASE%/Journal		Applications/Journal
 
-ECHO Setting up private Config directory
-git init Config
-copy "Template Files\Email Templates" "Config\Email Templates\"
-copy "Template Files\Word Templates"  "Config\Word Templates\"
+ECHO Setting up private Config directory...
+git init -q Config
+mkdir "Config\Email Templates\" 2>nul
+mkdir "Config\Word Templates\"  2>nul
+copy "Template Files\Email Templates" "Config\Email Templates\" >nul
+copy "Template Files\Word Templates"  "Config\Word Templates\"  >nul
 
-ECHO Building Configurator
-"%MSBuild%" "Configurator/ShomreiTorah-Configurator.sln" /target:rebuild /property:Configuration=Release
-Configurator\bin\Release\Configurator.exe
+ECHO Building Configurator...
+"%MSBuild%" "Setup\Configurator\ShomreiTorah-Configurator.sln" /target:rebuild /property:Configuration=Release /noconlog /nologo /v:q
+start Setup\Configurator\bin\Release\Configurator.exe
 
-popd
+POPD
+ENDLOCAL
