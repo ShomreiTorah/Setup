e42f7af6bde231c5d8681cb1c4ed224e1c7b50e7
SLaks <Dev@SLaks.net>
2016-07-14 21:04:30 -0400

    Add ImportedPayments table

---
+++ SQL Scripts/10-PaymentImport.sql
@@ -0,0 +1,14 @@
+﻿--This file creates tables used by the optional payment importer
+--plugin for the billing system.
+--It depends on Core and Billing.
+
+DROP TABLE Billing.ImportedPayments;
+CREATE TABLE Billing.ImportedPayments (
+	ImportedPaymentId UNIQUEIDENTIFIER	NOT NULL	ROWGUIDCOL	PRIMARY KEY DEFAULT(newid()),
+	PaymentId		UNIQUEIDENTIFIER	NOT NULL	UNIQUE		REFERENCES Billing.Payments(PaymentId) ON DELETE CASCADE,
+	Source			NVARCHAR(64)		NOT NULL,
+	ExternalId		NVARCHAR(64)		NOT NULL,
+	DateImported	DATETIME			NOT NULL	DEFAULT getdate(),
+	ImportingUser	NVARCHAR(32)		NOT NULL,
+	[RowVersion]	RowVersion
+);
